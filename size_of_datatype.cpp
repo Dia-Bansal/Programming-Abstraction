3ca2bb5e51f43d782669ee4810405dd700f18739
Dia-Bansal <78194499+Dia-Bansal@users.noreply.github.com>
2022-07-05 21:41:57 +0530

    Print size of data types program

---
+++ size_of_datatype.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+    cout<<"Size of integer "<<sizeof(int)<<endl;
+    cout<<"Size of character "<<sizeof(char)<<endl;
+    cout<<"Size of double "<<sizeof(double)<<endl;
+    cout<<"Size of long "<<sizeof(long)<<endl;
+    cout<<"Size of float "<<sizeof(float)<<endl;
+    cout<<"Size of bool "<<sizeof(bool)<<endl;
+    cout<<"Size of short "<<sizeof(short);
+    return 0;
+}
