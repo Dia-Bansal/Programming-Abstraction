31da4d4bee62cba1b9d803c65fcbc4220f10bf63
Dia-Bansal <78194499+Dia-Bansal@users.noreply.github.com>
2022-07-06 10:19:46 +0530

    Update size_of_datatype.cpp

--- size_of_datatype.cpp
+++ size_of_datatype.cpp
@@ -1,12 +1,12 @@
 #include<iostream>
 using namespace std;
 int main(){
-    cout<<"Size of integer "<<sizeof(int)<<endl;
-    cout<<"Size of character "<<sizeof(char)<<endl;
-    cout<<"Size of double "<<sizeof(double)<<endl;
-    cout<<"Size of long "<<sizeof(long)<<endl;
-    cout<<"Size of float "<<sizeof(float)<<endl;
-    cout<<"Size of bool "<<sizeof(bool)<<endl;
-    cout<<"Size of short "<<sizeof(short);
+    cout<<"Size of integer "<<sizeof(int)<<" bytes"<<endl;
+    cout<<"Size of character "<<sizeof(char)<<" bytes"<<endl;
+    cout<<"Size of double "<<sizeof(double)<<" bytes"<<endl;
+    cout<<"Size of long "<<sizeof(long)<<" bytes"<<endl;
+    cout<<"Size of float "<<sizeof(float)<<" bytes"<<endl;
+    cout<<"Size of bool "<<sizeof(bool)<<" bytes"<<endl;
+    cout<<"Size of short "<<sizeof(short)<<" bytes";
     return 0;
 }
